7c7046b481dd0df17392e84d65cc5b39654136c0
Jumoke Olaleye <olaleyesilver@gmail.com>
2022-09-16 01:00:24 +0100

    Initial Commit

---
+++ Dont ask me for money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dont ask me for money
+//
+//  Created by mac on 10/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
